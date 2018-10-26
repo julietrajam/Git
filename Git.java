b359a3833c4bf3d8897ea60b673346cdda4acb16
julietrajam <juliet.ramasamy@gmail.com>
2018-10-26 09:58:12 +0530

    Added a func

--- Git.java
+++ Git.java
@@ -1,5 +1,15 @@
 package Proj;
 
 public class Git {
+	
 
+	public void add()
+	{
+		System.out.println("Addition");
+	}
+	public static void main(String[] args) {
+		Git o1=new Git();
+		o1.add();
+	}
+	
 }
